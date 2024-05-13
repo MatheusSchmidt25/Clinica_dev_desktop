075326fb780b19d4f93bce961be44bc99ee1dee7
MatheusSchmidt25 <126730492+MatheusSchmidt25@users.noreply.github.com>
2024-05-13 19:28:51 -0300

    Add files via upload

---
+++ Clinica/src/test/java/br/unipar/programacaoweb/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipar.programacaoweb.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
